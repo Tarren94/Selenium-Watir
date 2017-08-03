fd8766edbda02c810d9d169d1b3585e676c2d49a
Tarren Patel <tarren.patel@hotmail.co.uk>
2017-08-03 13:37:52 +0100

    recent add

--- homework/form_code.rb
+++ homework/form_code.rb
@@ -3,7 +3,7 @@ require 'watir'
 class Form
 
     def initialize
-        @driver = Watir::Browser.new :chrome, profile: profile
+        @driver = Watir::Browser.new :chrome
         @driver.driver.manage.timeouts.implicit_wait = 10
         
     end
@@ -26,101 +26,89 @@ class Form
     def first_name
         fn = @driver.text_field name: 'firstname'
         fn.exists?
-        fn.set 'Tarren'
-        fn.value
+        fn
     end
 
     def last_name
         ln = @driver.text_field name: 'lastname'
         ln.exists?
-        ln.set 'Patel'
-        ln.value
+        ln
     end
 
     def gender
         radiobtn = @driver.radio id: 'sex-0'
         radiobtn.exists?
-        radiobtn.set
-        radiobtn.set?
+        radiobtn
 
     end
     
     def years_exp
         radiobtn = @driver.radio id: 'exp-0'
         radiobtn.exists?
-        radiobtn.set
-        radiobtn.set?
+        radiobtn
     end
    
     def date
         date_input = @driver.text_field id: 'datepicker'
         date_input.exists?
-        date_input.set '06/10/1994'
-        date_input.value
+        date_input
     end
 
     def profession1
         check = @driver.checkbox id: 'profession-0'
         check.exists?
-        check.set
-        check.set?
+        check
     end
 
     def profession2
         check = @driver.checkbox id: 'profession-1'
         check.exists?
-        check.set
-        check.set?
+        check
     end
 
-    def download_framework
-        download_directory = "#{Dir.pwd}/desktop/SDET9/downloads"
-        download_directory.tr!('/', '\\') if Selenium::WebDriver::Platform.windows?
+    # def download_framework
+    #     download_directory = "#{Dir.pwd}/desktop/SDET9/downloads"
+    #     download_directory.tr!('/', '\\') if Selenium::WebDriver::Platform.windows?
 
-        profile = Selenium::WebDriver::Chrome::Profile.new
-        profile[ 'download.prompt_for_download'] = false
-        profile['download.default_directory'] = download_directory
-
-        
-    end
+    #     profile = Selenium::WebDriver::Chrome::Profile.new
+    #     profile[ 'download.prompt_for_download'] = false
+    #     profile['download.default_directory'] = download_directory 
+    # end
 
     def automation_tool1
         check = @driver.checkbox id: 'tool-0'
         check.exists?
-        check.set
-        check.set?
+        check
     end
 
     def automation_tool2
         check = @driver.checkbox id: 'tool-1'
         check.exists?
-        check.set
-        check.set?
+        check
     end
 
     def automation_tool3
         check = @driver.checkbox id: 'tool-2'
         check.exists?
-        check.set
-        check.set?
+        check
     end    
 
     def drop_down_list
         drop = @driver.select_list id: 'continents'
-        drop.select 'Antartica'
-        drop.selected_options
+        drop.exists?
+        drop
     end
 
     def selenium_commands
         s = @driver.select_list id: 'selenium_commands'
-        s.select 'Wait Commands'
-        s.selected_options
+        s.exists?
+        s
     end
 
     def button
         btn = @driver.button id: 'submit'
         btn.exists?
-        btn.click
+        btn
     end
 
 end
--- homework/spec/form_spec.rb
+++ homework/spec/form_spec.rb
@@ -20,8 +20,72 @@ describe Form do
         expect(@driver.current_url).to eq ('http://toolsqa.com/automation-practice-form/')
     end
 
+    it "Verify that user can input their first name." do
+        expect(@driver.first_name.exists?).to be (true)
+        @driver.first_name.set ('Tarren')
+        expect(@driver.first_name.value).to eq ('Tarren')
+    end
 
+    it "Verify that user can input their last name." do
+        expect(@driver.last_name.exists?).to be (true)
+        @driver.last_name.set ('Patel')
+        expect(@driver.last_name.value).to eq ('Patel')
+    end
 
+    it "Verify that user can select a gender" do
+        expect(@driver.gender.exists?).to be (true)
+        @driver.gender.set
+        expect(@driver.gender.set?).to be (true)
+    end
 
+    it "Verify that user can select the number of years of experience they have" do
+        expect(@driver.years_exp.exists?).to be (true)
+        @driver.years_exp.set
+        expect(@driver.years_exp.set?).to be (true)
+    end
+
+    it "Verify that user can input a date" do
+        expect(@driver.date.exists?).to be (true)
+        @driver.date.set ('06/10/1994')
+        expect(@driver.date.value).to eq ('06/10/1994')
+    end
+
+    it "Verify that user can tick their profession" do
+        expect(@driver.profession1.exists?).to be (true)
+        @driver.profession1.set
+        expect(@driver.profession1.set?).to be (true)
+        expect(@driver.profession2.exists?).to be (true)
+        @driver.profession2.set
+        expect(@driver.profession2.set?).to be (true)
+    end
+
+    it "Verify that user can tick the Automation tools" do
+        expect(@driver.automation_tool1.exists?).to be (true)
+        @driver.automation_tool1.set
+        expect(@driver.automation_tool1.set?).to be (true)
+        expect(@driver.automation_tool2.exists?).to be (true)
+        @driver.automation_tool2.set
+        expect(@driver.automation_tool2.set?).to be (true)
+        expect(@driver.automation_tool3.exists?).to be (true)
+        @driver.automation_tool3.set
+        expect(@driver.automation_tool3.set?).to be (true)
+    end
+
+    it "Verify that user is able to select a Continent" do
+        expect(@driver.drop_down_list.exists?).to be (true)
+        @driver.drop_down_list.select 'Antartica'
+        # expect(@driver.drop_down_list.selected_options).to be (true)
+    end
+
+    it "Verify that user is able to select a Selenium Command" do
+        expect(@driver.selenium_commands.exists?).to be (true)
+        @driver.selenium_commands.select 'Wait Commands'
+        # expect(@driver.selenium_commands.selected_options).to be (true)
+    end
+
+    # it "Verify that user can click the button" do
+    #     expect(@driver.button.exists?).to be (true)
+    #     @driver.button.click
+    #     expect(@driver.current_url).to eq ('')
 
 end          
